99054e5ffed74f711e1bc0a61f01e21e0795398e
Yasin <136503768+theyasinsahin@users.noreply.github.com>
2024-04-26 19:49:49 +0300

    First mission completed

---
+++ MinOklidMesafesi/main.py
@@ -0,0 +1,23 @@
+import math
+
+# Noktaların Tanımlanması
+points = [(5, 10), (3, 4), (6, 7), (7, 8)]
+
+# Öklid Mesafesi İçin Bir Fonksiyon Yazma
+def euclideanDistance(point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+    distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    return distance
+
+# Mesafelerin Hesaplanması
+distances = []
+for i in range(len(points)):
+    for j in range(i+1, len(points)):
+        distance = euclideanDistance(points[i], points[j])
+        distances.append(distance)
+
+# Minimum Mesafenin Bulunması
+min_distance = min(distances)
+print("Minimum mesafe:", min_distance)
+
